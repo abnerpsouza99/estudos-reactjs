14712b0b8450500f3b696ef3b0a1b880fd9b093f
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-03-03 22:08:22 -0300

    Adds components with your styles

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,5 +1,7 @@
+import "./App.css";
 import React from 'react'
 
+import Card from './components/layout/Card';
 import Random from './components/basics/Random';
 import First from './components/basics/First';
 import WithParameter from './components/basics/WithParameter';
@@ -7,15 +9,24 @@ import Fragments from './components/basics/Fragments';
 
 export default () => {
   return (
-    <div>
-      <Random min={2} max={5}/>
+    <div className="App">
       <h1>Fundamentos Reactjs</h1>
-      <Fragments />
-      <WithParameter
-        title="Situação aluno"
-        student="Abner" grade={3.5} />
-      <First />
-
+      <div className="Cards">
+        <Card title="#04 - Desafio Aleatório">
+          <Random min={2} max={5} />
+        </Card>
+        <Card title="#03 - Desafio Fragmento">
+          <Fragments />
+        </Card>
+        <Card title="#02 - Desafio Com Parâmetro">
+          <WithParameter
+            title="Situação aluno"
+            student="Abner" grade={3.5} />
+        </Card>
+        <Card title="#01 - Desafio Primeiro Componente">
+          <First />
+        </Card>
+      </div>
     </div>
   )
 }
---
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './Card.css';
+
+export default function (props) {
+  return (
+    <div className="Card">
+      <div className="Title">{props.title}</div>
+      <div className="Content">
+        {props.children}
+      </div>
+    </div>
+  ) 
+  
+}
