81de52029be66d1e9004c95adb39a6543aea3703
abnerpsouza99 <abnertecnicosenac@gmail.com>
2021-03-03 22:54:52 -0300

    Exercise about children in Reactjs

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -1,6 +1,8 @@
 import "./App.css";
 import React from 'react'
 
+import Family from './components/basics/Family';
+import FamilyMember from './components/basics/FamilyMember';
 import Card from './components/layout/Card';
 import Random from './components/basics/Random';
 import First from './components/basics/First';
@@ -12,18 +14,25 @@ export default () => {
     <div className="App">
       <h1>Fundamentos Reactjs</h1>
       <div className="Cards">
-        <Card title="#04 - Desafio Aleatório">
+        <Card title="#05 - Desafio com Filhos" color="#FFC33C">
+          <Family surname="Souza">
+            <FamilyMember name="Abner"/>
+            <FamilyMember name="Juliana"/>
+            <FamilyMember name="Matheus"/>
+          </Family>
+        </Card>
+        <Card title="#04 - Desafio Aleatório" color="#080">
           <Random min={2} max={5} />
         </Card>
-        <Card title="#03 - Desafio Fragmento">
+        <Card title="#03 - Desafio Fragmento" color="#69D2E7">
           <Fragments />
         </Card>
-        <Card title="#02 - Desafio Com Parâmetro">
+        <Card title="#02 - Desafio Com Parâmetro" color="#542733">
           <WithParameter
             title="Situação aluno"
             student="Abner" grade={3.5} />
         </Card>
-        <Card title="#01 - Desafio Primeiro Componente">
+        <Card title="#01 - Desafio Primeiro Componente" color="#DC403B">
           <First />
         </Card>
       </div>
--- fundamentos-react/src/components/layout/Card.jsx
+++ fundamentos-react/src/components/layout/Card.jsx
@@ -3,13 +3,18 @@ import React from 'react';
 import './Card.css';
 
 export default function (props) {
+  const cardStyle = {
+    backgroundColor: props.color || "rebeccapurple",
+    borderColor: props.color || "rebeccapurple"
+  }
+
   return (
-    <div className="Card">
+    <div className="Card" style={cardStyle}>
       <div className="Title">{props.title}</div>
       <div className="Content">
         {props.children}
       </div>
     </div>
-  ) 
+  ); 
   
 }
